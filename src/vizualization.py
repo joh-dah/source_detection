881815ce2cb477fc181792948280291a94a065ae
conradhalle <conradhalle@gmail.com>
2023-06-01 13:38:47 +0200

    simplified the methods in vizualization.py

--- src/vizualization.py
+++ src/vizualization.py
@@ -1,130 +1,145 @@
+from typing import Union
 import networkx as nx
 from matplotlib import pyplot as plt
+from matplotlib.colors import Colormap
 from torch_geometric.utils.convert import from_networkx
 import src.data_creation as dc
 from pathlib import Path
 import src.constants as const
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap
+from ndlib.models.DiffusionModel import DiffusionModel
 
 
-def plot_graph_with_colors(g, colors, title, layout="spring"):
+def plot_graph_with_colors(
+    g: nx.Graph,
+    node_values: np.ndarray,
+    max_colored_value: int,
+    title: str,
+    cmap: Union[Colormap, str] = "viridis",
+    layout: any = nx.kamada_kawai_layout,
+):
     """
-    Plots graph and colors the nodes according to the given colors.
-    :param g: networkx graph
-    :param colors: list of colors
-    :param title: title of the plot
-    :param layout: layout of the plot
+    Plots graph and colors nodes according to node_values.
+    :param g: graph
+    :param node_values: values for nodes
+    :param max_colored_value: highest node value that should be mapped to a unique color
+    :param title: title of plot
+    :param cmap: colormap to use
+    :param layout: graph plotting layout to use
     """
     Path(const.FIGURES_PATH).mkdir(parents=True, exist_ok=True)
 
-    seed = const.SEED
-    if layout == "spring":
-        pos = nx.spring_layout(g, seed=seed)
-    elif layout == "circular":
-        pos = nx.circular_layout(g, seed=seed)
-    else:
-        raise AssertionError("Unknown layout")
+    pos = layout(g)
 
     plt.figure(figsize=(6, 4))
-    nx.draw(g, pos=pos, with_labels=True, node_color=colors, node_size=150)
-    plt.savefig(f"{const.FIGURES_PATH}/{title}.png")
-    plt.close()
 
-def plot_matching_graph(g, matching, new_edges, title="matching_graph", layout="spring"):
-    Path(const.FIGURES_PATH).mkdir(parents=True, exist_ok=True)
-
-    seed = const.SEED
-    if layout == "spring":
-        pos = nx.spring_layout(g, seed=seed)
-    elif layout == "circular":
-        pos = nx.circular_layout(g, seed=seed)
-    else:
-        raise AssertionError("Unknown layout")
-
-    plt.figure(figsize=(6, 4))
-    edge_colors = ["green" if edge in matching else "red" if edge in new_edges else "black" for edge in g.edges]
-    colors = ["red" if node[0] == "s" else "blue" for node in g.nodes]
-    nx.draw(g, pos=pos, with_labels=True, node_color=colors, edge_color=edge_colors, node_size=150)
-    nx.draw_networkx_edge_labels(g, pos, edge_labels=nx.get_edge_attributes(g, "weight"))
-    plt.savefig(f"{const.FIGURES_PATH}/{title}.png")
-    plt.close()
-
-
-def get_colors_for_infection_status(infection_status):
-    """
-    Returns colors for nodes based on their infection status.
-    :param infection_status: dict with node as key and infection status as value
-    :return: list of colors
-    """
-    colors = []
-    for _, i in infection_status.items():
-        if i == 0:
-            colors.append("blue")
-        elif i == 1:
-            colors.append("red")
-        elif i == 2:
-            colors.append("gray")
-    return colors
-
-
-def hex_to_RGB(hex):
-    """
-    Convert hex color to RGB.
-    e.g: "#FFFFFF" -> [255,255,255]
-    """
-    return [int(hex[i : i + 2], 16) for i in range(1, 6, 2)]
-
-
-def RGB_to_hex(RGB):
-    """
-    Convert RGB to hex color.
-    [255,255,255] -> "#FFFFFF"
-    """
-    RGB = [int(x) for x in RGB]
-    return "#" + "".join(
-        ["0{0:x}".format(v) if v < 16 else "{0:x}".format(v) for v in RGB]
+    node_values = np.clip(node_values, 0, max_colored_value)
+    nx.draw(
+        g,
+        pos=pos,
+        with_labels=True,
+        node_color=node_values,
+        node_size=150,
+        cmap=cmap,
+        vmin=0,
+        vmax=max_colored_value,
     )
+    # add colorbar with custom int ticks
+    sm = plt.cm.ScalarMappable(
+        cmap=cmap, norm=plt.Normalize(vmin=0, vmax=max_colored_value)
+    )
+    colorbar = plt.colorbar(sm, shrink=0.9)
+    colorbar.set_ticks(np.arange(0, max_colored_value + 1, 1))
+    colorbar.set_ticklabels(np.arange(0, max_colored_value + 1, 1))
+
+    plt.savefig(f"{const.FIGURES_PATH}/{title}.png")
+    plt.close()
 
 
-def linear_gradient(start_hex, finish_hex="#FFFFFF", n=10):
-    """
-    returns a gradient list of (n) colors between two hex colors.
-    start_hex and finish_hex should be the full six-digit color string,
-    inlcuding the number sign ("#FFFFFF")
-    """
-    s = hex_to_RGB(start_hex)
-    f = hex_to_RGB(finish_hex)
-    RGB_list = [s]
-    for t in range(1, n):
-        curr_vector = [
-            int(s[j] + (float(t) / (n - 1)) * (f[j] - s[j])) for j in range(3)
-        ]
-        RGB_list.append(curr_vector)
-    return [RGB_to_hex(RGB) for RGB in RGB_list]
+def plot_matching_graph(g, matching, new_edges, title="matching_graph"):
+    """Plots the matching graph to debug the min-matching distance metric"""
+    Path(const.FIGURES_PATH).mkdir(parents=True, exist_ok=True)
+
+    pos = nx.kamada_kawai_layout(g)
+
+    plt.figure(figsize=(6, 4))
+    edge_colors = [
+        "green" if edge in matching else "red" if edge in new_edges else "black"
+        for edge in g.edges
+    ]
+    colors = ["red" if node[0] == "s" else "blue" for node in g.nodes]
+    nx.draw(
+        g,
+        pos=pos,
+        with_labels=True,
+        node_color=colors,
+        edge_color=edge_colors,
+        node_size=150,
+    )
+    nx.draw_networkx_edge_labels(
+        g, pos, edge_labels=nx.get_edge_attributes(g, "weight")
+    )
+    plt.savefig(f"{const.FIGURES_PATH}/{title}.png")
+    plt.close()
 
 
 def plot_predictions(
-    prop_model, ranked_predictions, title, layout="spring", colored_nodes=7
+    prop_model: DiffusionModel,
+    node_values: np.ndarray,
+    title: str,
 ):
     """
     Plots the initial and the current graph with their infection status.
     Additionally, a graph is plotted with the colors of the nodes representing the predicted likelihood of beeing source.
-    :param prop_models: list of propagation models
-    :param ranked_predictions: list of ranked predictions
-    :param layout: layout of the plot
+    :param prop_model: propagation model
+    :param node_values: an array of values for each node, representing some kind of prediction (i.e. likelihood of beeing source or distance to source)
     """
     g = nx.Graph()
     g.add_nodes_from(prop_model.graph.nodes)
     g.add_edges_from(prop_model.graph.edges)
 
-    colors = get_colors_for_infection_status(prop_model.initial_status)
-    plot_graph_with_colors(g, colors, f"initial_{title}", layout)
+    # extract the maximum shortest path length from a source node to any other node -> used for coloring
+    source_nodes = np.where(
+        np.fromiter(prop_model.initial_status.values(), dtype=int) == 1
+    )[0]
+    max_distance_from_source = np.max(
+        [
+            np.max(
+                np.fromiter(
+                    nx.single_source_shortest_path_length(g, source_node).values(),
+                    dtype=int,
+                )
+            )
+            for source_node in source_nodes
+        ]
+    )
 
-    colors = get_colors_for_infection_status(prop_model.status)
-    plot_graph_with_colors(g, colors, f"current_{title}", layout)
+    sir_cmap = ListedColormap(["blue", "red", "gray"])
+    predictions_cmap = LinearSegmentedColormap.from_list("predictions", ["blue", "red"])
 
-    colors = ["#0000FF"] * len(g.nodes)
-    color_gradient = linear_gradient("#FF0000", "#0000FF", colored_nodes)
-    for i, node in enumerate(ranked_predictions[:colored_nodes]):
-        colors[node] = color_gradient[i]
-
-    plot_graph_with_colors(g, colors, f"prediction_{title}", layout)
+    # initial infection graph
+    plot_graph_with_colors(
+        g,
+        np.fromiter(prop_model.initial_status.values(), dtype=int),
+        2,
+        f"initial_{title}",
+        cmap=sir_cmap,
+    )
+    # current infection graph
+    plot_graph_with_colors(
+        g,
+        np.fromiter(prop_model.status.values(), dtype=int),
+        2,
+        f"current_{title}",
+        cmap=sir_cmap,
+    )
+    # predicted initial infection graph
+    print(max_distance_from_source)
+    plot_graph_with_colors(
+        g,
+        node_values,
+        max_distance_from_source,
+        f"prediction_{title}",
+        cmap=predictions_cmap,
+    )
