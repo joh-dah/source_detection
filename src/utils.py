9d1395c50cee810c6af1bb5bb5e4b2ed63b61975
joh-dah <johanna@dahlkemper.de>
2023-06-14 11:42:42 +0200

    Delete unused utils files

--- src/utils.py
+++ src/utils.py
@@ -1,48 +1,7 @@
 """utility functions for data loading and machine learning"""
-from os import listdir
-import pickle
 from pathlib import Path
-import numpy as np
 import torch
-from tqdm import tqdm
 import src.constants as const
-import src.visualization as vis
-import networkx as nx
-from ndlib.models.DiffusionModel import DiffusionModel
-from torch_geometric.data import Data
-from torch_geometric.utils import to_networkx
-
-
-def load_data(path, n_files=None):
-    """Loads data from path."""
-    data = []
-    print("Load Data:")
-    if n_files is None:
-        n_files = len(listdir(path))
-    print(listdir(path))
-    for file in tqdm(listdir(path)[:n_files]):
-        print(file)
-        data.append(torch.load(f"{path}/{file}"))
-    return data
-
-
-def extract_sources(prob_model) -> np.ndarray:
-    """Extracts sources from propagation model."""
-    initial_values = np.array(list(prob_model.initial_status.values()))
-    return np.where(initial_values == 1)[0]
-
-
-def get_ranked_source_predictions(predictions, n=None):
-    """
-    Return nodes ranked by predicted probability of beeing source. Selects the n nodes with the highest probability.
-    :param predictions: list of tuple predictions of nodes beeing source.
-    :param n: amount of nodes to return.
-    The second value of the tuple is the probability of the node beeing source.
-    :return: list of nodes ranked by predicted probability of beeing source.
-    """
-    if n is None:
-        n = predictions.shape[0]
-    return torch.topk(predictions.flatten(), n).indices
 
 
 def save_model(model, name):
@@ -66,61 +25,13 @@ def load_model(model, path):
     return model
 
 
-def visualize_results(
-    model: torch.nn.Module,
-    propagation_models: list[DiffusionModel],
-    prep_dataset: list[list[Data, torch.Tensor, torch.Tensor]],
-):
+def get_ranked_source_predictions(predictions, n=None):
     """
-    visualizes the predictions of the model.
-    :param model: The model on which predictions are made.
-    :param data_set: The data set to visualize on.
-    Contains the graph structure, the features and the labels.
+    Return nodes ranked by predicted probability of beeing source. Selects the n nodes with the highest probability.
+    :param predictions: list of predictions of nodes beeing source.
+    :param n: amount of nodes to return.
+    :return: list of nodes ranked by predicted probability of beeing source.
     """
-    print("visualize Results:")
-    for i, propagation_model in tqdm(enumerate(propagation_models)):
-        graph_structure, features, _ = prep_dataset[i]
-        predictions = model(features, graph_structure.edge_index)
-        ranked_predictions = get_ranked_source_predictions(predictions)
-        vis.plot_predictions(propagation_model, ranked_predictions, 7, title=f"_{i}")
-
-
-def visualize_results_gcnr(
-    model: torch.nn.Module,
-    propagation_models: list[DiffusionModel],
-    prep_dataset: list[list[Data, torch.Tensor, torch.Tensor]],
-):
-    """
-    visualizes the predictions of the model.
-    :param model: The model on which predictions are made.
-    :param data_set: The data set to visualize on.
-    Contains the graph structure, the features and the labels.
-    """
-    print("visualize Results:")
-    for i, propagation_model in tqdm(enumerate(propagation_models)):
-        graph_structure, features, _ = prep_dataset[i]
-        predictions = model(features, graph_structure.edge_index)
-
-        # convert predictions to np array of rounded integer values
-        predictions = np.round(predictions.detach().numpy().flatten()).astype(int)
-
-        # extract the maximum shortest path length from a source node to any other node -> used for coloring
-        source_nodes = np.where(
-            np.fromiter(propagation_model.initial_status.values(), dtype=int) == 1
-        )[0]
-        g = to_networkx(graph_structure)
-        max_distance_from_source = np.max(
-            [
-                np.max(
-                    np.fromiter(
-                        nx.single_source_shortest_path_length(g, source_node).values(),
-                        dtype=int,
-                    )
-                )
-                for source_node in source_nodes
-            ]
-        )
-
-        vis.plot_predictions(
-            propagation_model, predictions, max_distance_from_source, title=f"_{i}"
-        )
+    if n is None:
+        n = predictions.shape[0]
+    return torch.topk(predictions.flatten(), n).indices
