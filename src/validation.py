8df6660f3cd8b1663ffd50d0afafa7a9a0be7ae4
joh-dah <johanna@dahlkemper.de>
2023-07-05 22:34:12 +0200

    initial commit

--- src/validation.py
+++ src/validation.py
@@ -434,7 +434,7 @@ def main():
     metrics_dict["supervised"] = supervised_metrics(
         pred_labels, raw_val_data, model_name, dataset_name=dataset
     )
-    # metrics_dict["unsupervised"] = unsupervised_metrics(raw_val_data)
+    metrics_dict["unsupervised"] = unsupervised_metrics(raw_val_data)
     metrics_dict["data stats"] = data_stats(raw_val_data)
     metrics_dict["parameters"] = yaml.full_load(open("params.yaml", "r"))
     utils.save_metrics(metrics_dict, model_name, dataset)
