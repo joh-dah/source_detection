539bf5557e181ec0c0184ee6567b1327c435e02e
cdfhalle <conradhalle@gmail.com>
2023-07-11 21:04:06 +0000

    make stuff faster by using more resources

--- architectures/GCNR.py
+++ architectures/GCNR.py
@@ -11,11 +11,11 @@ class GCNR(torch.nn.Module):
         self.conv = GCNConv(const.HIDDEN_SIZE, const.HIDDEN_SIZE)
         self.classifier = torch.nn.Linear(const.HIDDEN_SIZE, 1)
 
-    def forward(self, x, edge_index):
-        h = self.conv_first(x, edge_index)
+    def forward(self, data):
+        h = self.conv_first(data.x, data.edge_index)
         h = h.relu()
         for i in range(1, const.LAYERS):
-            h = self.conv(h, edge_index)
+            h = self.conv(h, data.edge_index)
             h = h.relu()
         out = self.classifier(h)
         return out
